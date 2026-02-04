6fcfceb2a908570eba31d01916f6645f2fdefe80
mourhafkz <mourhafkazzaz@gmail.com>
2026-02-04 02:01:25 +0100

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -1,10 +1,12 @@
+install.packages(c("plumber", "jsonlite", "TAM"))
+
 library(plumber)
 
-# Load your API script
+# Load your API
 r <- plumb("api.R")
 
-# Use the PORT from Render (or default to 8080 locally)
+# Use Render-provided PORT
 port <- as.numeric(Sys.getenv("PORT", 8080))
 
-# Run the API
-r$run(host = "0.0.0.0", port = port)
+# Start the server
+r$run(host="0.0.0.0", port=port)
